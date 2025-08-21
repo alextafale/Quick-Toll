a016ae535142bebf75fbe6824a744955d61eeb3f
alex_tafale <tafoyaalex32@gmail.com>
2025-08-21 10:43:56 -0600

    LogIn without design

--- src/LogInScreen.js
+++ src/LogInScreen.js
@@ -2,7 +2,18 @@ import {View, Text, SafeAreaView, TouchableOpacity, StyleSheet} from 'react-nati
 export const LogInScreen = ({navigation}) => {
     return (
         <SafeAreaView >
-          
+          <View style={styles.container}>
+            <Text style={styles.title}>Log In</Text>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Email"   value={correo}  style={styles.input} />
+                <TextInput placeholder="Password" style={styles.input} secureTextEntry= {true} />
+            </View>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text style={styles.buttonText}>Iniciar Sesión</Text>
+            </TouchableOpacity>
+            </View>
+          </View>
         </SafeAreaView>
     );
 }
--- src/SignUpScreen.js
+++ src/SignUpScreen.js
@@ -4,22 +4,15 @@ const SignUpScreen = ({ navigation }) => {
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.tittle}>
-                        Unete a Quick Toll
-                    </Text>
+                <View style={styles.inputscontainer}>
+                    <TextInput style={styles.inputs} placeholder='Nombre' value='nombre' onChangeText={setNombre}/>
+                    <TextInput placeholder='Correo' value='correo'/>
+                    <TextInput placeholder='Contraseña' value='contraseña' secureTextEntry={true}/>
                 </View>
-                <View style={styles.inputs}>
-                    <TextInput 
-                        placeholder='Correo'
-                        value='correo'
-                    />
-                    <TextInput 
-                        placeholder='Contraseña'
-                        value='contraseña'
-                        secureTextEntry={true}
-                    />
-
+                <View style={styles.buttoncontainer}>
+                    <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Usuario registrado exitosamente')}>
+                        <Text style={styles.buttontext}>Registrarse</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </SafeAreaView>
