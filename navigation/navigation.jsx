55764c674ea2d929b94e59e747354dd9981bbfa1
alextafale <tafoyaalex32@gmail.com>
2025-08-26 16:12:46 -0600

    All the screens

--- navigation/navigation.jsx
+++ navigation/navigation.jsx
@@ -7,12 +7,17 @@ import {PaymentsScreen} from '../src/PaymentsScreen';
 import {SettingsScreen} from '../src/SettingsScreen';
 import {SignUpScreen} from '../src/SignUpScreen';
 import {SplashScreen} from '../src/SplashScreen';
-
+import {AddBalancesScreen} from '../src/AddBalancesScreen';
+import {ContactUsScreen} from '../src/ContactUsScreen';
+import {ForgottenScreen} from '../src/ForgottenScreen';
+import {HelpCenterScreen} from '../src/HelpCenterScreen';
+import { PaymentsMethodsScreen } from "../src/PaymentsMethodsScreen";
+import {VehicleScreen} from '../src/VehicleScreen';
 
 const AppNavigator = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Vinicio">    
+            <Stack.Navigator initialRouteName="SplashScreen">    
                 <Stack.Screen component={SplashScreen} options={{headerShown: false}}/>
                 <Stack.Screen component={HomeScreen} options={{headerShown: false}}/>
                 <Stack.Screen name="LogIn" component={LogInScreen} options= {{headerShown: false}}/>
@@ -20,6 +25,12 @@ const AppNavigator = () => {
                 <Stack.Screen name="History" component={HistoryScreen} options= {{headerShown: false}}/>
                 <Stack.Screen name="Payments" component={PaymentsScreen} options= {{headerShown: false}}/>
                 <Stack.Screen name="Settings" component={SettingsScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="AddBalances" component={AddBalancesScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="ContactUs" component={ContactUsScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="Forgotten" component={ForgottenScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="HelpCenter" component={HelpCenterScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="PaymentsMethods" component={PaymentsMethodsScreen} options= {{headerShown: false}}/>
+                <Stack.Screen name="Vehicle" component={VehicleScreen} options= {{headerShown: false}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
